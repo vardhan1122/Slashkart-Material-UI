976d4d9495444b1c4f0b00a7bfe02d5d241cb73e
Govardhan Reddy <vardhan.js90@gmail.com>
2021-03-02 22:23:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+import SlashkartMatUI from './components/SlashkartMatUI';
+
+
+function App() {
+  return (
+    <div className="App">
+        <SlashkartMatUI/>  
+    </div>
+  );
+}
+
+export default App;
